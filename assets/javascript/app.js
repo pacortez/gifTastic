b4baf34f7235111761e9f5a461f676b00aa7a1a1
Paulina Cortez <paulina.a.cortez@gmail.com>
2019-04-18 15:59:42 -0700

    organized script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,32 @@
-//Problems: 
-
-var topics = ["bengal cats", "dogs", "flamingos", "peacocks", "zebras", "tarantulas", "chinchilla", "sugar glider", "stingray"]
+var animals = ["bengal cats", "dogs", "flamingos", "peacocks", "zebras", "tarantulas", "chinchilla", "sugar glider", "stingray"]
 var still; 
 var animate; 
 
-// Your app should take the topics in this array and create buttons in your HTML.
-//    * Try using a loop that appends a button for each string in the array.
-//from Week 6, Activity 10
-
+//Creating buttons from each item in the animals array.   
 function renderButtons() {
     $("#buttons").empty();
-    for (var i = 0; i < topics.length; i++) {
+    for (var i = 0; i < animals.length; i++) {
         var b = $("<button>");
         b.addClass("animal");
-        b.attr("data-animal", topics[i]);
-        b.text(topics[i]);
+        b.attr("data-animal", animals[i]);
+        b.text(animals[i]);
         $("#buttons").append(b);
     }
 }
 
 renderButtons();
 
-// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY 
-// API and place them on the page.
+//On click event that allows the user to add their own animal button
+$("#add-animal").on("click", function(event) {
+    event.preventDefault();
+    var animal = $("#animal-input").val().trim();
+    animals.push(animal);
+    renderButtons();
+    $("#animal-input").val("")
+})
 
+// When the user clicks on an animal button, the page grabs 10 non-animated gif images from the GIPHY 
+// API and places them on the page along with their title, rating, and an option to add it to a favorites list.
 $(document).on("click", ".animal", function(event) {
     event.preventDefault();
     var animal = $(this).attr("data-animal");
@@ -40,9 +43,18 @@ $(document).on("click", ".animal", function(event) {
             
             var title = results[i].title;
             var rating = results[i].rating;
+
             var animalDiv = $("<div>");
+            animalDiv.attr("id", "animal-div");
+
             var p = $("<p>").html("Title: " + title + "<br> Rating: " + rating);
+            
             var animalImage = $("<img>");
+            animalImage.attr("src", results[i].images.fixed_width_still.url);
+            animalImage.attr("data-state", "still");
+            animalImage.attr("id", "toAnimate");
+            animalImage.attr("data-still", results[i].images.fixed_width_still.url);
+            animalImage.attr("data-animate", results[i].images.fixed_width.url);
 
             var favorites = $("<button>");
             favorites.addClass("favorite");
@@ -50,14 +62,6 @@ $(document).on("click", ".animal", function(event) {
             favorites.attr("data-src", results[i].images.fixed_width_small.url);
             favorites.attr("data-title", results[i].title);
 
-            animalImage.attr("src", results[i].images.fixed_width_still.url);
-            animalImage.attr("data-state", "still");
-            animalImage.attr("id", "toAnimate");
-            animalImage.attr("data-still", results[i].images.fixed_width_still.url);
-            animalImage.attr("data-animate", results[i].images.fixed_width.url);
-
-            
-            
             animalDiv.append(animalImage);
             animalDiv.append(p);
             animalDiv.append(favorites);
@@ -66,15 +70,30 @@ $(document).on("click", ".animal", function(event) {
     }) 
 })
 
+// When the user clicks one of the still GIPHY images, the gif animates. 
+// If the user clicks the gif again, it stops playing.
+$(document).on("click", "#toAnimate", function() {
+    var state = $(this).attr("data-state");
+
+    if(state === "still") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+    }
+    else {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));        
+    }
+})
+
+//a variable named favArray that grabs the items in localStorage and parses them back into objects.
 var favArray = JSON.parse(localStorage.getItem("favArray"));
 if (favArray === null) {
     favArray = [];
   }
 
-  console.log(favArray);
-  
-
-  function renderFavs(){
+//creates a function named renderFavs that will take the items favorited, give them attributes, 
+//and place them in a container
+function renderFavs(){
     $('#favorites').empty()
     for (let i = 0; i < favArray.length; i++) {
         var favContainer = $("<figure>");
@@ -91,18 +110,9 @@ if (favArray === null) {
     }
   };
 
-  renderFavs();
+renderFavs();
 
-$(document).on("click", ".delete", function() {
-    var index = $(this).attr("data-index");
-    favArray.splice(index, 1);
-    renderFavs()
-    localStorage.setItem("favArray", JSON.stringify(favArray));
-    // extract the object you want to delete from the favArray - splice()
-    // you want store favArray back to localStore
-    // renderFavs()
-})
-
+//on click event that will add the gify clicked to the array favArray and it will re-renderFavs
 $(document).on("click", ".favorite", function() {
     event.preventDefault();
     var favSrc = $(this).attr("data-src");
@@ -115,50 +125,13 @@ $(document).on("click", ".favorite", function() {
 
     favArray.push(newFav);
     renderFavs()
+    localStorage.setItem("favArray", JSON.stringify(favArray)); 
+})
+
+//This on click event enables the user to delete a gif from their favorites list
+$(document).on("click", ".delete", function() {
+    var index = $(this).attr("data-index");
+    favArray.splice(index, 1);
+    renderFavs();
     localStorage.setItem("favArray", JSON.stringify(favArray));
-
-    // var favTitleandSrc = favTitle + favSrc;
-    // localStorage.setItem("title and src", favTitleandSrc);
-    // $("#favorites").html(favTitleandSrc);
-    
-    // localStorage.setItem("title", favTitle);
-    // localStorage.setItem("src", favSrc);
-
-
-    
-
 })
-
-
-// When the user clicks one of the still GIPHY images, the gif should animate. 
-// If the user clicks the gif again, it should stop playing.
-$(document).on("click", "#toAnimate", function() {
-                
-    var state = $(this).attr("data-state");
-
-    if(state === "still") {
-        $(this).attr("data-state", "animate");
-        $(this).attr("src", $(this).attr("data-animate"));
-    }
-    else {
-        $(this).attr("data-state", "still");
-        $(this).attr("src", $(this).attr("data-still"));        
-    }
-})
-
-
-
-// Add a form to your page takes the value from a user input box and adds it into your `topics` array. 
-// Then make a function call that takes each topic in the array remakes the buttons on the page.
-
-
-
-$("#add-animal").on("click", function(event) {
-    event.preventDefault();
-    var animal = $("#animal-input").val().trim();
-    topics.push(animal);
-    renderButtons();
-    $("#animal-input").val("")
-
-})
-
